ab375b0034152b362b529a5a6b226dd97d0d5d40
Bonk578-max <mbryce385@gmail.com>
2026-01-06 16:24:56 -0800

    d

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -17,8 +17,10 @@ public class MathController : ControllerBase
     // Add two numbers
     [HttpGet]
     [Route("Add/{number1}/{number2}")]
-    public int AddNumbers(int number1, int number2)
+    public string AddNumbers(int number1, int number2)
     {
-        return number1 + number2;
+        int sum = number1 + number2;
+        return $"The sum of {number1} and {number2} is {sum}.";
     }
 }
+
